ca103e3e0728148a16a38b132d5f2d217d90abf3
AKASH CHEERLA <akashcvs007@gmail.com>
2024-04-05 08:38:40 -0400

    Update main.py
    added permission

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 import routers.audio_file as audio
 import routers.text_input as text
+from fastapi.middleware.cors import CORSMiddleware
 
 tags_metadata = [
     {
@@ -19,6 +20,18 @@ app = FastAPI(
     description="API for fri der man",
     version="1.0.0",
     openapi_tags=tags_metadata
+    
 )
+
+origins = ["*"]
+
+app.add_middleware ( 
+	CORSMiddleware, 
+	allow_origins=origins, 
+	allow_credentials=True, 
+	allow_methods=[""], 
+	allow_headers=[""],
+)
+
 app.include_router(audio.router)
 app.include_router(text.router)
